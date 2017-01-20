96dd88bb11e628c1c77ce4cace37c517e78a1379
Alex Melagrano <alexmelagrano@gmail.com>
2017-01-19 23:19:48 -0500

    moved footer and header Sass out to their component folders

--- components/header/navHeader.controller.js
+++ components/header/navHeader.controller.js
@@ -3,6 +3,6 @@
 angular.module('DarkSky')
     .controller('HeaderController', function ($scope) {
     
-   // It's just a footer. What do you want it to do, make a sandwich *while* riding a bike? 
+   // It's just a header. What do you want it to do, make a sandwich *while* riding a bike? 
     
 });
